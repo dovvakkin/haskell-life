1d3059e528a85926b7bf6ceb008a3bbcc3308a09
y.kozhevnikov <dovvakkin@gmail.com>
2020-04-13 12:07:48 +0300

    add some comments

--- src/Life.hs
+++ src/Life.hs
@@ -25,33 +25,43 @@ get_coordinate_on_torus :: Cell -> Cell
 get_coordinate_on_torus (cx, cy) = ((cx + field_width) `mod` field_width,
                                    (cy + field_height) `mod` field_height)
 
+-- | Return coordinates on torus of all neighbours of given Cell
 get_neighbours :: Cell -> [Cell]
 get_neighbours c@(cx, cy) = Prelude.map get_coordinate_on_torus $
                           [ (i, j) | i <- [cx - 1 .. cx + 1], j <- [cy - 1 .. cy + 1],
                           (i, j) /= c
                           ]
 
+-- | Return set of neighbours for all Cells from Field
 get_all_neighbours :: Field -> [Cell]
 get_all_neighbours f = Data.Set.foldl (\a b -> a ++ get_neighbours b ) [] f
 
+-- | Return Cells and number it's occurences in [Cells]
 list_to_count :: [Cell] -> [(Cell, Int)]
 list_to_count ls = Data.Map.toList (Data.Map.fromListWith (+) [(l, 1) | l <- ls])
 
+-- | Return
 field_to_counter :: Field -> [(Cell, Int)]
 field_to_counter f = list_to_count $get_all_neighbours f
 
+-- | Return True if in Cell with Int neighbours should born life else False
 is_born_with_neighbours :: (Cell, Int) -> Bool
 is_born_with_neighbours (_, n) = n `Data.Set.member` n_neighbours_to_born
 
+-- | Return True if in Cell with Int and life life should stay
 is_stay_with_neighbours :: (Cell, Int) -> Bool
 is_stay_with_neighbours (_, n) = n `Data.Set.member` n_neighbours_to_stay
 
+-- | Get list of Cells and it's counts as neighbours,
+--   Return next Field state
 counter_to_field :: [(Cell, Int)] -> Field -> Field
 counter_to_field l f = Data.Set.fromList
                      $ Prelude.map fst
 		     $ Prelude.filter (\x -> (is_stay_with_neighbours x) && ((fst x) `Data.Set.member` f) ||
 	                                     is_born_with_neighbours x) l
 
+-- | Get Field state,
+--   Return next Field state
 get_new_field :: Field -> Field
 get_new_field f = counter_to_field (field_to_counter f) f
 
@@ -65,6 +75,8 @@ createField = Data.Set.empty
 
 init_state = GS createField True
 
+-- | If life in Cell, return Field where no life in Cell,
+--   if no life in Cell, return Field where life in Cell
 modify_life :: Cell -> Field -> Field
 modify_life c f
 	| c `Data.Set.member` f = Data.Set.delete c f
